c3258f3a7a552072c94ab907eebb7a7d0d1539ef
Paolo Brasolin <paolo.brasolin@gmail.com>
2024-11-08 22:06:54 +0200

    Use agda-categories from local fork

--- flake.nix
+++ flake.nix
@@ -15,10 +15,22 @@
       in {
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
-            (agda.withPackages (ps: [
-              ps.standard-library
-              ps.agda-categories
-            ]))
+            (agda.withPackages
+              (ps: [
+                ps.standard-library
+                # ps.agda-categories
+                (ps.agda-categories.overrideAttrs (oldAttrs: {
+                  version = "local version";
+                  src = /Users/Brasolin/pb/agda-categories;
+                  # version = "bleeding edge";
+                  # src = fetchFromGitHub {
+                  #   repo = "agda-stdlib";
+                  #   owner = "agda";
+                  #   rev = "master";
+                  #   hash = "sha256-DfA1xE1ylk2O9Y9sE7fnK3T9/Zr2GMfmnzr9MHR5oHE=";
+                  # };
+                }))
+              ]))
           ];
         };
       }
